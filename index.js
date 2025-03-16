2c0a38ef6ae63790a63f8d62d04783551a2d29e6
Harsh Agarwal <alwaysharshgarg@gmail.com>
2025-03-16 22:01:36 +0530

    temp4

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
 // Serve static files (for frontend HTML)
 app.use(express.static(path.join(__dirname, "public")));
 
 // Simple route to serve the HTML file
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Test API endpoint (Responds immediately)
+app.post("/test-api", (req, res) => {
+  res.json({ message: "Test API response received!" });
 });
 
 // Start server
