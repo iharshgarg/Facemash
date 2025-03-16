bb0940c162ff4cf8e07b5d1dcee457833d01ab93
Harsh Agarwal <alwaysharshgarg@gmail.com>
2025-03-16 21:52:58 +0530

    temp3

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Simple route to test response
+// Serve static files (for frontend HTML)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Simple route to serve the HTML file
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Start server
